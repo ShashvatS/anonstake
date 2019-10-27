a4f964599fe7f2c7519b69fc00761b44dca66aa9
Shashvat Srivastava <shashvat.sri@gmail.com>
2019-10-26 21:16:48 -0400

    progress...

--- libfft/main.cpp
+++ libfft/main.cpp
@@ -2,6 +2,8 @@
 // Created by shashvat on 10/26/19.
 //
 
+void nothing() {}
+
 int run() {
     return 1 + 2 + 3;
 }
---
+++ rust/anonstake/build.rs
@@ -0,0 +1,13 @@
+extern crate cmake;
+
+use cmake::Config;
+use std::process::Command;
+
+fn main() {
+
+
+//    let dst = Config::new("./../../").build_target("fft").uses_cxx11().build();
+//
+//    println!("cargo:rustc-link-search=native={}", dst.display());
+//    println!("cargo:rustc-link-lib=static=libfft");
+}

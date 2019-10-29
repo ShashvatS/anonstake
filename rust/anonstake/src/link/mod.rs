9d9e925fd1cbd98819477372ee14469fe743100f
Shashvat Srivastava <shashvat.sri@gmail.com>
2019-10-28 21:10:01 -0400

    added an experiment

--- rust/anonstake/src/link/mod.rs
+++ rust/anonstake/src/link/mod.rs
@@ -1,4 +1,25 @@
+use rand::thread_rng;
+use pairing::bls12_381::Bls12;
+use ff::{Field, PrimeField, ScalarEngine};
+
 extern {
     pub fn hello_world();
     pub fn init();
+    pub fn check_value(u: *mut u8);
+}
+
+#[test]
+pub fn experiment() {
+    let rng = &mut thread_rng();
+    let mut num = <Bls12 as ScalarEngine>::Fr::random(rng);
+    let ptr: *mut <Bls12 as ScalarEngine>::Fr = &mut num;
+
+    unsafe {
+        hello_world();
+        init();
+    }
+    println!("{}", num.into_repr());
+    unsafe {
+        check_value(ptr as *mut u8);
+    }
 }
